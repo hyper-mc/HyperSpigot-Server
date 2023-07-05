pvginkel/Tweakkit-Server

Configurable save-on-stop-only for UserCache

src/main/java/net/minecraft/server/MinecraftServer.java
@@ -419,6 +419,13 @@ public abstract class MinecraftServer implements ICommandListener, Runnable, IMo
             if (this.l.d()) {
                 this.l.e();
             }
+            // Spigot start
+            if( org.spigotmc.SpigotConfig.saveUserCacheOnStopOnly )
+            {
+                i.info("Saving usercache.json");
+                this.X.c();
+            }
+            //Spigot end
         }
     }
 

hyper-mc/HyperSpigot-Server

src/main/java/net/minecraft/server/MinecraftServer.java
@@ -486,7 +486,13 @@ public abstract class MinecraftServer implements Runnable, ICommandListener, IAs
             if (this.n.d()) {
                 this.n.e();
             }
-
+            // Spigot start
+            if( org.spigotmc.SpigotConfig.saveUserCacheOnStopOnly )
+            {
+                LOGGER.info("Saving usercache.json");
+                this.Z.c();
+            }
+            //Spigot end
         }
     }
 